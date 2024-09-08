922877294c233a976e67c9e2cb522171cf705620
devwithmehar <meharsahil207@gmail.com>
2024-09-08 18:26:15 -0500

    client secret and client id is added via env

--- src/config.ts
+++ src/config.ts
@@ -4,3 +4,7 @@ export const environment = process.env.NODE_ENV;
 export const port = process.env.PORT || 8000;
 
 export const corsUrl = process.env.CORS_URL;
+
+export const redirectUri = process.env.REDIRECT_URI;
+export const client_id = process.env.CLIENT_ID;
+export const client_secret = process.env.CLIENT_SECRET;
