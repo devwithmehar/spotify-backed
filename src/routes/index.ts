6d04e7581a98a3d811c4ccbb52e8600c5fb74dc5
devwithmehar <meharsahil207@gmail.com>
2024-09-11 22:43:06 -0500

    Refresh Router is imported

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import login from './acess/login.js';
+import refresh from './acess/refresh.js';
 
 const router = express.Router();
 
 
 router.use('/login', login);
+router.use('/refresh',refresh);
 
 
 
