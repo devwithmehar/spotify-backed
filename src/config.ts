97648b8db9431c31a667b64d165d570b9f1156c1
devwithmehar <meharsahil207@gmail.com>
2024-09-11 23:15:29 -0500

    client_url is added in config file

--- src/config.ts
+++ src/config.ts
@@ -8,3 +8,5 @@ export const corsUrl = process.env.CORS_URL;
 export const redirectUri = process.env.REDIRECT_URI;
 export const client_id = process.env.CLIENT_ID;
 export const client_secret = process.env.CLIENT_SECRET;
+
+export const client_url = process.env.CLIENT_URL;
