4daadc7200b985271298e5f659ef4b67ed1c2f38
devwithmehar <meharsahil207@gmail.com>
2024-09-11 23:20:32 -0500

    Get API for route ('/') is made

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,16 @@
-import express from 'express';
+import { Request, Response, Router } from 'express'; 
 import login from './acess/login.js';
 import refresh from './acess/refresh.js';
+import { client_url } from '../config.js';
 
-const router = express.Router();
+const router = Router();
+
+router.get('/', (req: Request, res: Response) => {
+    if (!client_url) {
+        throw new Error('client_url is not defined');
+    }
+    res.redirect(client_url);
+})
 
 
 router.use('/login', login);
