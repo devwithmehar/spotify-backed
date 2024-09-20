1073a4b411cbb83e0e2d46b860dfbe95b9c7b5ec
devwithmehar <meharsahil207@gmail.com>
2024-09-20 11:54:52 -0500

    login is imported in main router

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
+const login = require('./acess/login');
+
+
 var router = express.Router();
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.use('/login', login);
 
 module.exports = router;
